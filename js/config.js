d738ec1edbe55b70b600be4b0734c126f546f1c5
Nick Ursetta <nickursetta@MacBook-Pro.local>
2020-09-09 22:59:53 -0600

    create js folder and button

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_qRRf4q8R3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5bik0jg4vqdrir2a92187h9g8j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
